01c4718eef3d82e871f0026e3eebc33cb48f86e8
Anchieta N. Albano <anchietaalbano@alu.ufc.br>
2024-10-09 20:43:29 -0300

    add IF e Operadores

--- src/main/kotlin/aulas/ControleDeFluxos/EstruturaDeDecisaoIF.kt
+++ src/main/kotlin/aulas/ControleDeFluxos/EstruturaDeDecisaoIF.kt
@@ -1,2 +1,21 @@
 package aulas.ControleDeFluxos
 
+fun main() {
+    // Classifique as pessoas através da faixa etária
+    // Idoso - 60 anos acima | Adulto 21 a 59 anos | Jovem entre 13 a 20
+    // Criança 12 anos abaixo
+    var idade: Int = 80
+
+    if (idade < 13) {
+        println("Criança")
+    }
+    else if(idade >= 13 && idade <= 20) {
+        println("Jovem")
+    }
+    else if(idade >= 21 && idade <= 59) {
+        println("Adulto")
+    }
+    else{
+        println("Idoso")
+    }
+}
---
+++ src/main/kotlin/aulas/ControleDeFluxos/Operadores.kt
@@ -0,0 +1,26 @@
+package aulas.ControleDeFluxos
+
+fun main() {
+
+    var b:Boolean = false
+    b = !(b)
+    println(b)
+}
+
+/*
+* OPERADORES LOGICOS
+*       && -> E
+*       || -> OU
+*       ! -> Negação
+* OPERADORES MATEMATICOS
+*       + , - , * , / e %
+* OBS: Igual ao Java
+* OPERADORES COMPARATIVOS
+*       > Maior que
+*       >= Maior ou igual
+*       < Menor
+*       <= Menor ou igual
+*       == igualdade, Comparação
+*       = Atribuição
+*       != Diferente
+* */
