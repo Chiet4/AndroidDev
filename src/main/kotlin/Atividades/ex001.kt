45196e3583999b1bac31cd3b7a82da8fdf6b6350
Anchieta N. Albano <anchietaalbano@alu.ufc.br>
2024-10-09 16:45:33 -0300

    Unidade 2: Variaveis

---
+++ src/main/kotlin/Atividades/ex001.kt
@@ -0,0 +1,7 @@
+package Atividades
+
+fun main() {
+    println("Olá mundo")
+    println("Meu nome é Anchieta")
+    println("Minha idade é 21 anos")
+}
---
+++ src/main/kotlin/aulas/ControleDeFluxos/Variaveis.kt
@@ -0,0 +1,39 @@
+package aulas.ControleDeFluxos
+
+// var - tipo de variavel mutavel
+// val - tipo constante, imutavel
+
+fun main() {
+    var x: Int = 12
+    val y: Int
+    var a: Float = 12.32f
+    var b: Double = 12.43
+    var c: Long = 101010100101
+    var d: Char = 'F'
+    var e: String = "palavras"
+    var f: Short = 100
+    var g: Byte = 0b11
+    var h: Byte = 0x10
+    var i: UInt = 12u // unsigned
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
+
+    println("------------------------------------------")
+    println("Capacidade de variaveis")
+    capacidade()
+}
+
+fun capacidade() {
+    println("ULong máximo: ${ULong.MAX_VALUE} ULong mínimo: ${ULong.MIN_VALUE}")
+    println("UInt máximo: ${UInt.MAX_VALUE} UInt mínimo: ${UInt.MIN_VALUE}")
+    println("UShort máximo: ${UShort.MAX_VALUE} UShort mínimo: ${UShort.MIN_VALUE}")
+    println("Long máximo: ${Long.MAX_VALUE} Long mínimo: ${Long.MIN_VALUE}")
+}
